627961c44d79cf1fa6cb528a63dd0c490b1f1516
David Reed <reedswork@proton.me>
2023-05-21 02:54:47 -0400

    just redirect

--- serve/dogeware.user.js
+++ serve/dogeware.user.js
@@ -1,4 +1,4 @@
 alert(
   "Krunker Cheat Loader is outdated. Use Krunker Sketch. https://sketch.sys32.dev/"
 );
-window.open("https://sketch.sys32.dev/");
+location.href = "https://sketch.sys32.dev/";
